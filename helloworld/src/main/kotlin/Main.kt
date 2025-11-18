4a266dad23ff89d248b9c27c0aad53644f05443a
Nikita_Poddubnyi <piddubniimikita@gmail.com>
2025-11-18 15:16:48 +0200

    Update Main.kt

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -35,6 +35,10 @@ suspend fun serverDataCalculate(strList: List<String>): Double = coroutineScope 
     tanh(maxValue.toDouble())
 }
 
+
+
+
+
 fun main() = runBlocking {
     val data = listOf("x0", "x1", "x2", "x3", "x4", "x5")
     val result = serverDataCalculate(data)
