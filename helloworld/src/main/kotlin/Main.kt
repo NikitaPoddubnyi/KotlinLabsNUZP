28e50c54965d73fbb7e0acc89b830375b508fe8b
Nikita_Poddubnyi <piddubniimikita@gmail.com>
2025-11-18 14:18:05 +0200

    lab1

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,25 +1,22 @@
 package org.example
 
-fun seed(): String = "NikitaPoddubnyi"
-
+fun seed(): String = "Piddubnyi_Nikita"
 fun labNumber(): Int = 1
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 
-    
     var kitty = "Васько"
     kitty += " \uD83D\uDC31"
-    var age = 7
+    val age = 7
     println("Кошеня №1 - $kitty віком $age років")
 
     val catName: String = "Мурзик \uD83D\uDC08"
     val weight: Float = 3.5f
     println("Кошеня №2 - $catName з вагою $weight кг")
 
-    var secCat = "Рудий"
-    secCat += " \uD83D\uDC06"
-    var newAge = 6
-    val newWeight = 8.2f
-    println("Кошеня №3 - $secCat віком $newAge років та вагою $newWeight кг")
+    val catName2: String = "Рудий \uD83D\uDC06"
+    val weight2: Float = 8.2f
+    val age2 = 6
+    println("Кошеня №3 - $catName2 віком $age2 та вагою $weight2 кг")
 }
