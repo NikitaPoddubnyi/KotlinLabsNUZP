69f496a479117ec55b00106d5ce0145d1357a884
Nikita_Poddubnyi <piddubniimikita@gmail.com>
2025-11-18 14:50:12 +0200

    Update Main.kt

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun labNumber(): Int = 1
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-    
+
     var kitty = "Васько"
     kitty += " \uD83D\uDC31"
     val age = 7
@@ -14,6 +14,9 @@ fun main(args: Array<String>) {
     val weight: Float = 3.5f
     println("Кошеня №2 - $catName з вагою $weight кг")
 
+
+
+    
     val catName2: String = "Рудий \uD83D\uDC06"
     val weight2: Float = 8.2f
     val age2 = 6
